cddc4c4bce0defce0a8e849e870dbff2673c063a
JosephDemarest <joseph.demarest@cix.csi.cuny.edu>
2022-09-10 22:58:20 -0400

    updated

--- main.py
+++ main.py
@@ -24,17 +24,15 @@ class SubListen(threading.Thread):
     while True:
         for submission in subreddit.stream.submissions():
             if re.search(JAX_REGEX, submission.title, re.IGNORECASE):
-                submission.reply("ACTIVE: Jax enters Evasion, a defensive stance, for 2 seconds, causing all non-turret basic attacks against him to be dodged. Jax also takes 25% reduced damage from all champion area of effect abilities. Counter Strike can be recast after 1 second. At the end of the duration, Jax stuns all nearby enemies for 1 second and deals physical damage to them, increased by 20% for each attack dodged, up to a 100% increase.")
+                submission.reply("string")
             elif re.search(IRELIA_REGEX, submission.title, re.IGNORECASE):
-                submission.reply(
-                    "I fucking love Irelia's butt, I want to bury myself between those cheeks and just inhale the raw scent of the stank as it slowly crushes my skull")
+                submission.reply("string")
             elif re.search(LUX_REGEX, submission.title, re.IGNORECASE):
-                submission.reply("Lux drives me absolutely wild and I need to vent She's just a dumb, slutty blonde that's super peppy and cheery, but as soon as you gank her lane and grab her hair she just turns into a sparkly-eyed, blushing whore for cock. Every time I win my lane against Lux I get a testosterone overload. I have to look at her hentai to let it all out. Your shield might protect you from my Q's but not this goddamn dick. Thank you for coming to my ted talk")
+                submission.reply("string")
             elif re.search(TEEMO_REGEX, submission.title, re.IGNORECASE):
-                submission.reply(
-                    "As a teemo main at a respectably high elo, this game is hard to watch. Literally cringing at some of these mistakes. If you actually want to learn teemo PM me (im gold 3 24lp) I also do coaching")
+                submission.reply("string")
             elif re.search(ZOE_REGEX, submission.title, re.IGNORECASE):
-                submission.reply("I hope Zoe wins xD. I’m a Zoe main and she’s just so fun!! People get so trolled by the bubble, and her voice lines are so cute like when she sings about chocolate cake LOL! She’s super random but also smarter than she looks, just like me xD")
+                submission.reply("string")
 
 
 def canreply(s):
