797f2d486c43711e70ac2ea8489097340d656b88
JosephDemarest <joseph.demarest@cix.csi.cuny.edu>
2022-09-11 22:01:18 -0400

    modifications

--- main.py
+++ main.py
@@ -3,38 +3,30 @@ import threading
 import praw
 
 BOT_USERNAME = "x"
-SUBREDDIT = "all"
-JAX_REGEX = r'[^a-z]jax[^a-z]|^jax'
-IRELIA_REGEX = r'[^a-z]irelia[^a-z]|^irelia'
-LUX_REGEX = r'[^a-z]lux[^a-z]|^lux'
-TEEMO_REGEX = r'[^a-z]teemo[^a-z]|^teemo'
-ZOE_REGEX = r'[^a-z]zoe[^a-z]|^zoe'
-reddit = praw.Reddit(
-    client_id="x",
-    client_secret="x",
-    password="x",
-    user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36",
-    username="x",
-)
 
-
-class SubListen(threading.Thread):
-    print('Started listening sub: ' + SUBREDDIT)
+def main():
+    reddit = praw.Reddit(
+        client_id="x",
+        client_secret="x",
+        password="x",
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36",
+        username="x",
+    )
+    SUBREDDIT = input("What subreddit would you like to listen in?:: ")
+    REGEX = input("Input Regex to reply to: ") 
+    reply = input("Input reply text: ")
+    
     subreddit = reddit.subreddit(SUBREDDIT)
-    while True:
-        for submission in subreddit.stream.submissions():
-            if re.search(JAX_REGEX, submission.title, re.IGNORECASE):
-                submission.reply("string")
-            elif re.search(IRELIA_REGEX, submission.title, re.IGNORECASE):
-                submission.reply("string")
-            elif re.search(LUX_REGEX, submission.title, re.IGNORECASE):
-                submission.reply("string")
-            elif re.search(TEEMO_REGEX, submission.title, re.IGNORECASE):
-                submission.reply("string")
-            elif re.search(ZOE_REGEX, submission.title, re.IGNORECASE):
-                submission.reply("string")
+    for submission in subreddit.stream.submissions(skip_existing=True):
+        parse_submission(submission,reply,REGEX)
 
+def parse_submission(submission, reply, REGEX):
+    if (re.search(REGEX, submission.title, re.IGNORECASE) and canreply(submission)):
+        submission.reply(reply)
+    if (re.search(REGEX, submission.comments, re.IGNORECASE) and canreply(submission)):
+        submission.reply(reply)
 
+    
 def canreply(s):
     if alreadyreplied(s):
         return False
@@ -63,7 +55,5 @@ def alreadyreplied(s):
 
     return False
 
-
 if __name__ == "__main__":
-    sublisten = SubListen()
-    sublisten.start()
+    main()
